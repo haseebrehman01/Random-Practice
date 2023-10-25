8cdd90da167e287a2b28f2d20f46ab973be5fced
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-10-25 09:09:38 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// array of object
+// var info = [
+//  [
+//         students = [
+//             { name: "haseeb" ,
+//                grade : "A+",
+//                perc : "90%"          
+//         },
+//             {name: "Rameez" ,
+//             grade : "b+",
+//             perc : "50%"  },
+//             {name: "ali" ,
+//             grade : "c",
+//             perc : "40%"  }
+//         ]
+//  ],
+//  [
+//     students2 = [
+//         { name: "haseeb" ,
+//            grade : "A+",
+//            perc : "90%"          
+//     },
+//         {name: "Rameez" ,
+//         grade : "b+",
+//         perc : "50%"  },
+//         {name: "ali" ,
+//         grade : "c",
+//         perc : "40%"  }
+//     ]
+// ]
+// ];
+// var con = info[1][0][0]["name"]
+// alert(con)
+//  random num between 1 and 100
+// var b = Math.floor(Math.random()*100)+1
+// console.log(b)
+// var b = Math.floor(Math.random()*5)+1
+// console.log(b)
